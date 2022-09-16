97470879eed84db6ded5a1970f02877f1fac65ec
Gautam Krishna M <charmingdeveloper@protonmail.com>
2022-09-16 18:23:38 +0530

    Some more changes

--- src/Component/HeadLog/HeadLog.js
+++ src/Component/HeadLog/HeadLog.js
@@ -1,43 +1,5 @@
 import React from "react";
 import "./headl.css";
 export default function HeadLog() {
-  return (
-    <div className="bg-[#0c192c] h-[25rem]">
-      <div className="bubble">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-        <span className=" hidden md:block"></span>
-      </div>
-    </div>
-  );
+  return <div className="bg-[#0c192c] h-[86vh]"></div>;
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import HeadLog from "./Component/HeadLog/HeadLog";
 import Navbar from "./Component/NavBar/navbar";
+import About from "./Component/About/About";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
     <HeadLog />
+    <About />
   </React.StrictMode>
 );
