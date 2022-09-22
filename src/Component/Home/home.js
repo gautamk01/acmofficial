1dc45178dcc51d988964889690e2a30f4037b7d3
Abhinand Arun <abhinandarun2015@gmail.com>
2022-09-23 01:18:13 +0530

    Implement News section on homepage

--- src/Component/Home/home.js
+++ src/Component/Home/home.js
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import HeadLog from "../HeadLog/HeadLog";
+import News from "../News/News";
 
 export default function main_page_component() {
   return (
     <div>
       <HeadLog />
       <About />
+      <News/>
     </div>
   );
 }
---
+++ src/Component/News/News.js
@@ -0,0 +1,22 @@
+import React from "react"
+import NewsCard from "./NewsCard";
+import './news.css'
+import Rectangle6 from '../../assets/Rectangle 6.png'
+import Rectangle7 from '../../assets/Rectangle 7.png'
+import Rectangle8 from '../../assets/Rectangle 8.png'
+
+
+function News() {
+    return (
+        <div className="font-poppins flex flex-col items-center">
+            <h1 className="font-semibold text-5xl text-center">Read Our News</h1>
+            <div className="news-container">
+                <NewsCard img={Rectangle6}/>
+                <NewsCard img={Rectangle7}/>
+                <NewsCard img={Rectangle8}/>
+            </div>
+        </div>
+    )
+}
+
+export default News
