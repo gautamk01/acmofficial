c5d48a04d50298fb42e3eed195cb85016dc20601
Gautam Krishna M <charmingdeveloper@protonmail.com>
2022-09-24 23:25:35 +0530

    Added some more alignment

--- src/Component/FacultyCard/Ecard.js
+++ src/Component/FacultyCard/Ecard.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 import "./Ecard.css";
 
 function Ecard() {
   const [title, setTitle] = useState(false);
   console.log(title);
   return (
-    <div className="h-[40%] w-[15%]  m-3  flex flex-wrap justify-center main">
+    <div className="h-[35%] w-[15%]  m-3   justify-center main">
       <div
-        className="h-[60%] w-[100%]  funCard  flex flex-wrap justify-center "
+        className="h-[60%] w-[100%]  funCard  flex flex-wrap  justify-center "
         onMouseEnter={() => {
           setTitle(true);
         }}
@@ -15,12 +18,25 @@ function Ecard() {
           setTitle(false);
         }}
       >
-        <div className="img_box"></div>
+        <div className="img_box ">
+          {title && (
+            <div className="relative top-[7.3rem] left-[10px] flex ">
+              <a href="http://www.google.com" className=" ml-6">
+                <BsLinkedin />
+              </a>
+              <a href="http://www.google.com" className="ml-6 ">
+                <BsInstagram />
+              </a>
+              <a href="http://www.google.com" className="ml-6">
+                <BsGithub />
+              </a>
+            </div>
+          )}
+        </div>
 
         <div className="ket text-center">
           <div>
-            {" "}
-            <h1 className=" font-bold">Gautam Krishna M</h1>
+            <h1 className=" mb-1 mt-2 font-bold">Gautam Krishna M</h1>
             <h1 className=" font-thin ">Senior Web Developer</h1>
           </div>
         </div>
--- src/Component/people/pep.js
+++ src/Component/people/pep.js
@@ -3,7 +3,17 @@ import Ecard from "../FacultyCard/Ecard";
 
 function PEP() {
   return (
-    <div className="h-[100vh] w-[100%]  bg-gradient-to-r from-white to-cyan-200 flex flex-wrap justify-center items-center">
+    <div className="h-[100vh] w-[100%] p-20  bg-gradient-to-r from-white to-cyan-200 flex flex-wrap justify-center">
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
+      <Ecard />
       <Ecard />
       <Ecard />
       <Ecard />
