6b89560ceea604b38c0ff799f1f730d0e549867b
Gautam Krishna M <charmingdeveloper@protonmail.com>
2022-09-25 07:46:31 +0530

    Some more code to commit

--- src/Component/Home/home.js
+++ src/Component/Home/home.js
@@ -4,15 +4,13 @@ import HeadLog from "../HeadLog/HeadLog";
 import News from "../News/News";
 import PEP from "../people/pep";
 
-
 export default function main_page_component() {
   return (
     <>
       <HeadLog />
       <About />
       <PEP />
-      <News/>
-      
+      <News />
     </>
   );
 }
