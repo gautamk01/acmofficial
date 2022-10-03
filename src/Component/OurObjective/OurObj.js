fcb22ef59b868eb266f48138190d9516456ef5d0
Akash <akashharikumar2013@gmail.com>
2022-10-03 17:41:34 +0530

    Made 'Our Objective' section responsive

--- src/Component/OurObjective/OurObj.js
+++ src/Component/OurObjective/OurObj.js
@@ -1,28 +1,22 @@
 import React from "react";
 function OurObj() {
   return (
-    <div>
-      <div class="grid grid-cols-2 gap-2 content-center p-20 bg-gradient-to-l addedbg2 ">
-        <div className="p-20">
-          <img src={require("../../assets/amrita-logo.png")} alt="ourobj"></img>
-        </div>
-
-        <div className="mt-40 p-10 ">
-          <p class="text-4xl font-bold mb-5">Our Objective</p>
-          <p className="mb-5">
-            The objective of the chapter to promote computer science as a field
-            of education and foster a sense of innovation and creativity among
-            computer enthusiasts.It's a hub for computer enthusiasts where they
-            meet, interact, learn from each other and nurture themselves in the
-            process.
-          </p>
-          <button class="p-3 bg-sky-400 rounded-md text-white text-sm transition ease-in-out hover:scale-105 hover:shadow-md">
+    <div>        
+      <div className="h-[100vh] w-[100%] grid grid-cols-1 md:grid-cols-2 gap-2 content-center justify-items-center font-poppins">
+        <img src={require('../../assets/amrita-logo.png')} alt="ourobj" className="hidden md:block h-[25rem] w-[25rem]"></img>
+        
+        <div className='p-12'>
+          <h1 className="font-bold w-[100%] text-6xl">Our Objective</h1>
+          <br></br>
+          <p className='font-normal text-lg text-slate-400'>The objective of the chapter to promote computer science as a field of education and foster a sense of innovation and creativity among computer enthusiasts.It's a hub for computer enthusiasts where they meet, interact, learn from each other and nurture themselves in the process.</p>
+          <br></br>
+          <button className="p-3 bg-sky-400 rounded-md text-white text-sm transition ease-in-out hover:scale-105 hover:shadow-md">
             Learn More
           </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default OurObj;
