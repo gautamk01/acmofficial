279b04f7575dfdfe45e90f2c87b0457163621305
Abhinand Arun <abhinandarun2015@gmail.com>
2022-10-04 21:28:48 +0530

    Refactor news section
    All news data is now stored in an object
    Simplified css
    Converted remaining css to tailwind
    Removed Read More link [Will implement in future update]

--- src/Component/News/News.js
+++ src/Component/News/News.js
@@ -1,21 +1,41 @@
 import React from "react";
 import NewsCard from "./NewsCard";
 import "./news.css";
-import Rectangle6 from "../../assets/Rectangle 6.png";
-import Rectangle7 from "../../assets/Rectangle 7.png";
-import Rectangle8 from "../../assets/Rectangle 8.png";
+import icpc_logo from "../../assets/icpc logo.jpg";
+import code_drills from "../../assets/codedrills.jpg";
+
+const news = [
+    {
+        src: icpc_logo,
+        desc: "The International Collegiate Programming Contest is an algorithmic programming contest for college students. Teams of three, representing their university, work to solve the most real-world problems, fostering collaboration, creativity, innovation, and the ability to perform under pressure. Through training and competition, teams challenge each other to raise the bar on the possible. Quite simply, it is the oldest, largest, and most prestigious programming contest in the world.",
+        author: "Admin",
+        date: "08 October 2022",
+    },
+    {
+        src: code_drills,
+        desc: "We, the ACM Student Chapter at Amritapuri, are glad to announce the launch of a CodeDrills Campus Chapter in our University and its onboarding session on 18th December!",
+        author: "Admin",
+        date: "18 December 2021 ",
+    },
+    {
+        src: icpc_logo,
+        desc: "Lorem ipsum dolor sit amet, consecte adipiscing elit ut aliquam. Lorem ipsum dolor sit amet, consecte adipiscing elit ut aliquam",
+        author: "Admin",
+        date: "09 April 2021 ",
+    }
+]
 
 function News() {
-  return (
-    <div className=" p-10 font-poppins flex flex-col items-center addedbg2">
-      <h1 className="font-semibold text-5xl text-center">Read Our News</h1>
-      <div className="news-container">
-        <NewsCard img={Rectangle6} />
-        <NewsCard img={Rectangle7} />
-        <NewsCard img={Rectangle8} />
-      </div>
-    </div>
-  );
+    return (
+        <div className=" p-10 font-poppins flex flex-col items-center addedbg2">
+            <h1 className="font-semibold text-5xl text-center">Read Our News</h1>
+            <div className="flex flex-col justify-evenly items-center  w-full gap-16 mt-8 md:flex-row md:items-stretch">
+                {news.map(data => (
+                    <NewsCard img={data.src} desc={data.desc} author={data.author} date={data.date}/>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default News;
--- src/Component/News/NewsCard.js
+++ src/Component/News/NewsCard.js
@@ -3,11 +3,10 @@ import './news.css'
 
 function NewsCard(props) {
     return (
-        <div className="news-card">
-            <img className="rounded-3xl  w-[250px]" src={props.img} alt=""/>
-            <div className="flex font-medium pt-5 gap-6"><p className="text-xs text-[#1E1C3199]"> 09 April 2022 </p><p className="text-xs text-[#1E1C3199]">by Admin</p></div>
-            <div className="flex flex-wrap"><p>Lorem ipsum dolor sit amet, consecte adipiscing elit ut aliquam </p></div>
-            <span className="text-[#4270EC] pt-2">Read More</span>
+        <div className="w-full max-w-[300px] flex flex-col gap-2 text-[0.875rem] leading-5">
+            <img className="rounded-3xl aspect-square" src={props.img} alt=""/>
+            <div className="flex font-medium pt-5 gap-6"><p className="text-xs text-[#1E1C3199]">{props.date}</p><p className="text-xs text-[#1E1C3199]">{"By " + props.author}</p></div>
+            <div className="flex flex-wrap"><p className={"description"}>{props.desc}</p></div>
         </div>
     );
 }
