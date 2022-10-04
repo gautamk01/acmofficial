dccc95e1493895919bb4e1f68f9fd74ab1a13b54
NiranjanNR <84635960+NiranjanNR@users.noreply.github.com>
2022-10-04 22:00:40 +0530

    Added Student Management

--- src/Component/Home/home.js
+++ src/Component/Home/home.js
@@ -4,6 +4,7 @@ import OurObj from "../OurObjective/OurObj";
 import News from "../News/News";
 import PEP from "../people/pep";
 import Footer from "../FooterComp/Footer";
+import StuManagement from "../StuManagement/StuManagement"
 export default function main_page_component() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function main_page_component() {
       <About />
       <OurObj />
       <PEP />
+      <StuManagement />
       <News />
       <Footer />
     </>
